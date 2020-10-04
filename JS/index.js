f0964dac39c2b444dbc531c49d73308955e59132
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-10-04 19:08:16 +0200

    Added jquery codes for toggling the services icons

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+   
+    
+    $("#s1").click(function(){
+        $(".design-desc").toggle();
+        });
+    $("#s2").click(function(){
+        $(".dev-desc").toggle();
+        });
+    $("#s3").click(function(){
+        $(".prod-desc").toggle();
+        });
+});
